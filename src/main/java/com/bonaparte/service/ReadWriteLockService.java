1ea18f18d06219016e6d4c5e116df39175cb89b8
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-10-06 16:30:13 +0800

    ReadWriteLock读写锁

--- src/main/java/com/bonaparte/service/ReadWriteLockService.java
+++ src/main/java/com/bonaparte/service/ReadWriteLockService.java
@@ -2,9 +2,28 @@ package com.bonaparte.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * Created by yangmingquan on 2018/10/4.
  */
 @Service
 public class ReadWriteLockService {
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public double chargeMoney = 0;
+
+    public void setChargeMoney(double chargeMoney){
+        readWriteLock.writeLock().lock();
+        this.chargeMoney = chargeMoney;
+        readWriteLock.writeLock().unlock();
+    }
+
+    public double getChargeMoney(double chargeMoney){
+        readWriteLock.readLock().lock();
+        double chargeMoneyTemp = chargeMoney;
+        readWriteLock.readLock().unlock();
+        return chargeMoney;
+    }
 }
